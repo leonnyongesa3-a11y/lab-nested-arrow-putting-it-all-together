058c9d3e2d2d6bc1f8b7b4c88f7b1a155057917f
Leon <leonnyongesa3@gmail.com>
2025-12-01 09:32:28 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+let userInfo = {
+  username: "user1",
+  password: "password123",
+};
+
+function createLoginTracker(userInfo) {
+  console.log(`userInfo: ${userInfo}`);
+  let attemptCount = 0;
+
+  return (LoginAttempt = (passwordAttempt) => {
+    attemptCount += 1;
+
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return "Login successful";
+    } else {if (attemptCount <= 3) {
+          return `Attempt ${attemptCount}: Login failed`;
+        } else {
+          if (attemptCount > 3) {
+            return "Account locked due to too many failed login attempts";
+          }
+        }
+      }
+    });
+  }
 
 
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
